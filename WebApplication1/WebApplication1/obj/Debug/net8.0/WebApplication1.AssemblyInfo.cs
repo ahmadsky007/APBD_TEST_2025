1772b69602fb6ab4e4b0298f8326bf4950103865
ahmadsky007 <107273027+ahmadsky007@users.noreply.github.com>
2025-05-12 14:21:04 +0200

    Commit

--- WebApplication1/WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
+++ WebApplication1/WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e51fdcb74ce2a93afcbf4d79dc68551ac9ecaac7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1f333cc5daeeb0e079f2a42a9f14dd553aff6936")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
